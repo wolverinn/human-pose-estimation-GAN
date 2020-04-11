fbab20ce82ef564bedf9a307f7d21cece48447a9
zhangzekuan <zhangzekuan@bytedance.com>
2020-04-11 15:35:20 +0800

    change to no 3d

--- src/config.py
+++ src/config.py
@@ -71,7 +71,9 @@ flags.DEFINE_string('model_dir', osp.join(base_dir, 'logs/'), 'Where model will 
 flags.DEFINE_integer('log_img_step', 1000, 'How often to visualize img during training')
 flags.DEFINE_integer('epoch', 75, '# of epochs to train')
 
-flags.DEFINE_list('datasets', ['lsp', 'lsp_ext', 'mpii', 'coco', 'mpi_inf_3dhp'],
+# flags.DEFINE_list('datasets', ['lsp', 'lsp_ext', 'mpii', 'coco', 'mpi_inf_3dhp'],
+#                           'datasets to use for training')
+flags.DEFINE_list('datasets', ['lsp', 'lsp_ext', 'mpii', 'coco'],
                           'datasets to use for training')
 flags.DEFINE_list('mocap_datasets', ['CMU', 'jointLim'],
                   'datasets to use for adversarial prior training')
@@ -82,7 +84,7 @@ flags.DEFINE_boolean(
     'if set, no adversarial prior is trained = monsters')
 
 flags.DEFINE_boolean(
-    'use_3d_label', True,
+    'use_3d_label', False,
     'Uses 3D labels if on.')
 
 # Hyper parameters:
