96a3bf6269ffd3cd9ee6ce8e0b30d29a355b5c35
zhangzekuan <zhangzekuan@bytedance.com>
2020-04-12 09:20:57 +0800

    update tf_records

--- auto.sh
+++ auto.sh
@@ -1,4 +1,6 @@
+# wget https://github.com/wolverinn/hmr2020/raw/master/auto.sh && bash auto.sh
 # tensorflow<=1.14
+# 确认src/main.py中使用的device是选gpu还是cpu
 
 mkdir hmr
 cd hmr/
@@ -20,14 +22,30 @@ cd ..
 pip3 install deepdish --user
 pip3 install chumpy --user
 
+# get train code from github
 wget https://github.com/wolverinn/hmr2020/archive/master.zip
 unzip master.zip
 rm master.zip
 mv ./hmr2020-master/src ./src
+mv ./hmr2020-master/start.sh ./start.sh
 rm -rf hmr2020-master
 
-tar -xf tf_records.tar.gz
-mv ./content/drive/My\ Drive/tf_records ./tf_records
+# get processed data tf_record
+wget https://github.com/wolverinn/hmr2020/releases/download/v1.0/tf1.tar.gz
+tar -xf tf1.tar.gz
+mv ./tf1 ./tf_records
 
-# 确认src/main.py中使用的device是选gpu还是cpu
-# 最后运行 python3 -m src.main 即可
+wget https://github.com/wolverinn/hmr2020/releases/download/v1.1/tf2.tar.gz
+tar -xf tf2.tar.gz
+mv ./mocap_neutrMosh ./tf_records/mocap_neutrMosh
+
+wget https://github.com/wolverinn/hmr2020/releases/download/v1.2/tf3.tar.gz
+tar -xf tf3.tar.gz
+mv ./coco_pre ./tf_records/coco
+
+wget https://github.com/wolverinn/hmr2020/releases/download/v1.2/tf4.tar.gz
+tar -xf tf4.tar.gz
+mv ./coco/* ./tf_records/coco/
+
+# 最后运行 python3 -m src.main 即可，或者使用：
+# bash start.sh
